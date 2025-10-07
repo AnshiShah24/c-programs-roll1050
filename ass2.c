acdf6a67094a7ab2381d525a0f971ea21128548f
AnshiShah24 <anshishah71@gmail.com>
2025-10-07 15:58:12 +0530

    ass2.c

--- ass2.c
+++ ass2.c
@@ -12,7 +12,7 @@ int main()
     {
         if (str[i] >= 'a' && str[i] <= 'z')
         {  
-            str[i] = str[i] - 32;            
+            str[i] -= 32;            
         }
     }
 

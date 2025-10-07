dae7da360d5ccbfc707d106ad0ae9e3f91704b84
AnshiShah24 <anshishah71@gmail.com>
2025-10-07 21:29:58 +0530

    ass13.c

--- ass13.c
+++ ass13.c
@@ -2,37 +2,11 @@
 #include<stdio.h>
 int main()
 {
-  anshi:
-  char ch;
   float miles, kilometer;
   printf("Enter the distance in miles:");
   scanf("%f",&miles);
   
   kilometer = miles * 1.60934;
   printf("%.2f miles = %.2f kilometer",miles,kilometer);
-     getchar();
-    printf("\n Do you want to continue?");
-    scanf("%c",&ch);
-  
-    if(ch=='y')
-    {
-      goto anshi;
-    }
-    else
-   {
-     printf("\n THANK YOU");
-   }
-     getchar();
-    printf("\n Do you want to continue?");
-    scanf("%c",&ch);
-  
-    if(ch=='y')
-    {
-      goto anshi;
-    }
-    else
-   {
-     printf("\n THANK YOU");
-   }
 return 0;
 }

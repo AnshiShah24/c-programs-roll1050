17a1488839f6f741e8b328d1b64a7eb7ad1306c0
AnshiShah24 <anshishah71@gmail.com>
2025-10-07 21:28:22 +0530

    ass11.c

--- ass11.c
+++ ass11.c
@@ -2,25 +2,11 @@
 #include<stdio.h>
 int main()
 {
-  anshi:
-  char ch;
   float fah,celcius;
   printf("Enter the temperature in fahrenheit:");
   scanf("%f",&fah);
   
   celcius=(fah-32)*5.0/9.0;
   printf("%.2f Fahrenheit = %.2f Celcius\n",fah,celcius); 
-     getchar();
-    printf("\n Do you want to continue?");
-    scanf("%c",&ch);
-  
-    if(ch=='y')
-    {
-      goto anshi;
-    }
-    else
-   {
-     printf("\n THANK YOU");
-   }
 return 0;
 }
